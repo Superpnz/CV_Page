b6132a0a3531ccee9b9c8f1e0f7c07632f2ae68e
Maxim_Anik <maksim9431@gmail.com>
2026-06-22 14:09:45 +0300

    chore: add certificate

--- slider.js
+++ slider.js
@@ -1,5 +1,6 @@
 const images = [
     "Assets/Udemy React Kalatilshchykau .webp",
+    "Assets/Сертификат itilogia.webp",
     "Assets/Сертификат_основы_алгоритмов_и_структур_данных.webp",
     "Assets/Сертификат_основы_цифровой_граматности.webp",
 ];
